bdeea72c5608e6bd5853b72bae09fcfceb2ef9c3
dashabuliak <g00489499@atu.ie>
2026-09-23 13:17:12 +0000

    first java file

---
+++ Welcome1.java
@@ -0,0 +1,14 @@
+public class Welcome1 {
+    public static void main (String[] args) {
+        // all code here 
+        /* Name: Daria
+        // Date: 21/09
+        // Desc: Example first java program */
+
+        System.out.println("First Java Program"); 
+        System.out.println("Another line of output");
+        System.out.print("Same line");
+        System.out.print("Still same line");
+        System.out.println();
+    }
+} 
